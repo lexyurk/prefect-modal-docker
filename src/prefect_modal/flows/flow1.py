7e6eac776b309619b588e57121a581af31724e98
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2025-02-09 19:24:44 +0100

    move no pydantic flow to other file

--- src/prefect_modal/flows/flow1.py
+++ src/prefect_modal/flows/flow1.py
@@ -28,11 +28,3 @@ def test_flow(args: MyModel):
     if args.goodbye:
         print(f"Goodbye {args.name}!")
 
-
-@flow(log_prints=True, name="test_flow_no_pydantic")
-def test_flow_no_pydantic(name: str, goodbye: bool = False):
-    print(f"Hello {name} from Prefect! 🤗")
-    run_my_flow(name)
-    print(f"pydantic version: {pydantic.__version__}")
-    if goodbye:
-        print(f"Goodbye {name}!")
---
+++ src/prefect_modal/flows/flow2.py
@@ -0,0 +1,23 @@
+from prefect import flow, task
+import pydantic
+
+
+@task()
+def do_print(param: str) -> None:
+    print("Doing the task")
+    print(param)
+
+
+@flow(log_prints=True)
+def run_my_flow(param: str) -> None:
+    print("flow 2")
+    do_print(param)
+
+
+@flow(log_prints=True, name="test_flow_no_pydantic")
+def test_flow_no_pydantic(name: str, goodbye: bool = False):
+    print(f"Hello {name} from Prefect! 🤗")
+    run_my_flow(name)
+    print(f"pydantic version: {pydantic.__version__}")
+    if goodbye:
+        print(f"Goodbye {name}!")
